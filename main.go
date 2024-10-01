00087a8dcd02f5ae913a527916a2c1497ef974ab
Elena Anikina <la2709@ya.ru>
2024-10-01 12:41:08 +0300

    feat: fan out fan in

--- main.go
+++ main.go
@@ -1,49 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"sync/atomic"
-)
-
-type Counter struct {
-	value       int
-	valueAtomic int64
-	sync.Mutex
+type Ingredient struct {
+	Name     string
+	Quantity string
 }
 
-func (c *Counter) Inc() {
-	c.Lock()
-	c.value++
-	c.Unlock()
+type Recipe struct {
+	Name        string
+	Ingredients []Ingredient
 }
 
-func (c *Counter) IncAtomic() {
-	atomic.AddInt64(&c.valueAtomic, 1)
-}
-
-func (c *Counter) Value() int {
-	return c.value
-}
-
-func (c *Counter) ValueAtomic() int64 {
-	return atomic.LoadInt64(&c.valueAtomic)
+func worker(recipes <-chan Recipe, done chan<- int) {
+	for recipe := range recipes {
+		done <- len(recipe.Ingredients)
+	}
 }
 
 func main() {
-	c := &Counter{}
-	var wg sync.WaitGroup
 
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			c.Inc()
-			c.IncAtomic()
-		}()
-	}
-	wg.Wait()
-
-	fmt.Println("Final counter value:", c.Value())
-	fmt.Println("Final counter value atomic:", c.ValueAtomic())
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"reflect"
+	"slices"
+	"sync"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	recipes := []Recipe{
+		{
+			Name: "Pizza",
+			Ingredients: []Ingredient{
+				{"Flour", "2 cups"},
+				{"Yeast", "1 packet"},
+				{"Water", "1 cup"},
+			},
+		},
+		{
+			Name: "Pasta",
+			Ingredients: []Ingredient{
+				{"Pasta", "200g"},
+				{"Water", "2 liters"},
+				{"Salt", "1 teaspoon"},
+				{"Olive oil", "1 tablespoon"},
+			},
+		},
+	}
+
+	recipeChannel := make(chan Recipe, len(recipes))
+	done := make(chan int, len(recipes))
+
+	var wg sync.WaitGroup
+	for w := 1; w <= 3; w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			worker(recipeChannel, done)
+		}()
+	}
+
+	go func() {
+		for _, recipe := range recipes {
+			recipeChannel <- recipe
+		}
+		close(recipeChannel)
+	}()
+
+	wg.Wait()
+	close(done)
+
+	expected := []int{3, 4}
+	results := make([]int, 0, len(recipes))
+	for result := range done {
+		results = append(results, result)
+	}
+
+	slices.Sort(results)
+
+	if !reflect.DeepEqual(expected, results) {
+		t.Errorf("Expected %v, got %v", expected, results)
+	}
+}
